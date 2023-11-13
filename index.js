3c0d4bdb7c965617dea48c9392e3fff325e01a4b
Brent <BrentC0328@gmail.com>
2023-11-13 16:50:27 -0500

    total right/wrong score updates with submit.

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const form = document.querySelector(".quizForm")
 const correctAnswer = ["A", "A", "A","A","A"]
+const result = document.querySelector(".result");
+
+
 form.addEventListener("submit", event => {
     event.preventDefault();
 
     let score = 0;
 
+    //gets answer to each question.
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value]
   
 
+    //counts answers and adds score up.
     userAnswers.forEach((answer, index) => {
         if (answer === correctAnswer[index]){
             score = score + 1;
@@ -15,4 +20,16 @@ form.addEventListener("submit", event => {
 
     });
     console.log(score);
+
+    //scrolls to top and shows the final score at the top.
+    scrollTo(0,0);
+    result.classList.remove("hide");
+
+    //querySelector will grab the first instance of "p" in the class, querySelectorAll will grab all references.
+    //textContent will then grab the text content in that div and allow me to assign it's value to whatever I want it to be.
+    //So then I can update it based off the score.
+
+    //throw a template literal in there and...
+    result.querySelector("p").textContent = `You scored ${score}/5`;
+
 });
